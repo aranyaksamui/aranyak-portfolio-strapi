a21b5893f641cc81354a21876889f359f0c14a61
Aranyak Samui <aranyaksamuiarya@gmail.com>
2025-07-02 21:28:22 +0530

    Initial commit, Strapi ready for first production deployment.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }: { env: any }) => (
+    {
+        upload: {
+            config: {
+                provider: "cloudinary",
+                providerOptions: {
+                    cloud_name: env("CLOUDINARY_NAME"),
+                    api_key: env("CLOUDINARY_KEY"),
+                    api_secret: env("CLOUDINARY_SECRET"),
+                },
+                actionOptions: {
+                    upload: {},
+                    uploadStream: {
+                        folder: env("CLOUDINARY_FOLDER", "aranyak_portfolio"),
+                        use_filename: true,
+                        unique_filename: false,
+                        overwrite: true,
+                        resource_type: "auto",
+                    },
+                    delete: {},
+                },
+            },
+        },
+    }
+);
